f79bf27e694f6d28b7936a9b6f3ba21a6f89fda0
JisuLim <sanola3@gmail.com>
2018-11-13 05:37:20 +0900

    DataBinding, Glide 추가

--- app/src/main/java/com/kotlin/insane/kpassignment/util/Strings.kt
+++ app/src/main/java/com/kotlin/insane/kpassignment/util/Strings.kt
@@ -1,3 +1,5 @@
 package com.kotlin.insane.kpassignment.util
 
 const val baseUrl = "http://api.openweathermap.org"
+const val cityId = "1835847"
+const val appId = "16b01403f276732d12cddb76fd386375"
